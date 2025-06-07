286d579bed6e5db1ddd05ee324a3a86d6c70fe58
hi0yoo <hi0yoo@douzone.com>
2025-06-07 22:06:17 +0900

    refactor: 주문번호 생성 컴포넌트 수정

--- modules/order/order-api/src/main/kotlin/me/hi0yoo/commerce/order/infrastructure/adapter/out/NextOrderNumberGeneratorAdapter.kt
+++ modules/order/order-api/src/main/kotlin/me/hi0yoo/commerce/order/infrastructure/adapter/out/NextOrderNumberGeneratorAdapter.kt
@@ -1,18 +1,15 @@
 package me.hi0yoo.commerce.order.infrastructure.adapter.out
 
 import me.hi0yoo.commerce.order.application.port.out.NextOrderNumberGeneratorPort
-import me.hi0yoo.commerce.order.config.OrderApiRedisScriptLoader
-import org.springframework.data.redis.core.StringRedisTemplate
+import me.hi0yoo.commerce.order.infrastructure.redis.UniqueSequencePerSecondProvider
 import org.springframework.stereotype.Component
 
 @Component
 class NextOrderNumberGeneratorAdapter(
-    private val redisTemplate: StringRedisTemplate,
-    private val orderApiRedisScriptLoader: OrderApiRedisScriptLoader,
+    private val uniqueSequencePerSecondProvider: UniqueSequencePerSecondProvider,
 ): NextOrderNumberGeneratorPort {
     override fun fetchFrom(yyyyMMddHHmmss: String): Long {
         val key = "order_seq:$yyyyMMddHHmmss"
-        val seq = redisTemplate.execute(orderApiRedisScriptLoader.script, listOf(key))
-        return seq
+        return uniqueSequencePerSecondProvider.nextSequenceOf(key)
     }
 }
--- modules/order/order-api/src/main/kotlin/me/hi0yoo/commerce/order/infrastructure/redis/UniqueSequencePerSecondProvider.kt
+++ modules/order/order-api/src/main/kotlin/me/hi0yoo/commerce/order/infrastructure/redis/UniqueSequencePerSecondProvider.kt
@@ -1,12 +1,15 @@
-package me.hi0yoo.commerce.order.config
+package me.hi0yoo.commerce.order.infrastructure.redis
 
 import jakarta.annotation.PostConstruct
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.script.DefaultRedisScript
 import org.springframework.stereotype.Component
 
 @Component
-class OrderApiRedisScriptLoader {
-    lateinit var script: DefaultRedisScript<Long>
+class UniqueSequencePerSecondProvider(
+    private val redisTemplate: RedisTemplate<String, String>
+) {
+    private lateinit var script: DefaultRedisScript<Long>
 
     @PostConstruct
     fun loadScript() {
@@ -16,11 +19,15 @@ class OrderApiRedisScriptLoader {
             local key = KEYS[1]
             local val = redis.call("INCR", key)
             if val == 1 then
-              redis.call("EXPIRE", key, 2)
+              redis.call("EXPIRE", key, 1)
             end
             return val
         """.trimIndent()
         )
         script.resultType = Long::class.java
+    }
+
+    fun nextSequenceOf(key: String): Long {
+        return redisTemplate.execute(script, listOf(key))
     }
 }
